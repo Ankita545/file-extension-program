9399dee4350a12e1c7cb62553073db5c2bf867a9
Ankita545 <66065953+Ankita545@users.noreply.github.com>
2020-05-28 16:34:39 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,10 @@
+#file extension
+file_name=input('input the filename: ')
+exten=file_name.split(".")    #splits the file name after '.' 
+print('The extension of the file is: ')
+if exten[-1]=='py':
+    print('python')
+elif exten[-1]=='cpp':
+    print('c++')
+elif exten[-1]=='c':
+    print('java')
